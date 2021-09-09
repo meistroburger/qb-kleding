a2fe87800002324a1e0a788c5af41f919fa2fc77
meistroburger <ea.bos@icloud.com>
2021-09-09 02:36:23 +0200

    Update

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-# qb-politiekleding
+## qb-politiekleding
+- Voor BestPixel hebben we een gemakkelijk script gemaakt waarmee we politiekleding als een item weergeven en bruikbaar maken. We werken momenteel nog aan het script, waardoor we adviseren deze NIET TE DOWNLOADEN totdat de uiteindelijke versie voltooid is.
+daarnaast is alles gekoppeld aan de kleding zoals wij deze ingesteld hebben en kan deze dus voor een ieder afwijken. Om meer informatie te krijgen over hoe je deze aanpast bekijk dan Credits/Links.
 
-> Een script om van Politiekleding, items te maken.
+> Images
+- Images dienen in qb-inventory geplaatst te worden
 
-## WIP
+> Shared.lua
+- Items zoals in shared.lua dienen toegevoegd te worden aan qb-core/shared.lua
+
+> Credits/Links
+- Credits (Original): [ArcadiaRoleplay](https://github.com/ArcadiaRoleplay/qb-bag)
+- Reworked/Script: MeistroBurger
+- Informatie: [SetPedComponentVariation](https://docs.fivem.net/natives/?_0x262B14F48D29DE80)
+- Informatie 2: [SetPedComponentVariation](http://www.kronzky.info/fivemwiki/index.php?title=SetPedComponentVariation)
---
+++ shared.lua
@@ -0,0 +1,132 @@
+	---------------------------
+	-- Politiekleding
+	---------------------------	
+	["politiekleding-aspirant"] = {
+		["name"] = "politiekleding-aspirant",
+		["label"] = "Politiekleding",
+		["weight"] = 100, 		
+		["type"] = "item", 		
+		["image"] = "pol_aspirant.png",
+		["unique"] = true,
+		["useable"] = true,
+		["shouldClose"] = false,   
+		["combinable"] = nil,   
+		["description"] = "Aspiranten Kleding"
+	},
+	
+	["politiekleding-surveillant"] = {
+		["name"] = "politiekleding-surveillant",
+		["label"] = "Politiekleding",
+		["weight"] = 100, 		
+		["type"] = "item", 		
+		["image"] = "pol_surveillant.png",
+		["unique"] = true,
+		["useable"] = true,
+		["shouldClose"] = false,   
+		["combinable"] = nil,   
+		["description"] = "Surveillanten Kleding"
+	},
+	
+	["politiekleding-agent"] = {
+		["name"] = "politiekleding-agent",
+		["label"] = "Politiekleding",
+		["weight"] = 100, 		
+		["type"] = "item", 		
+		["image"] = "pol_agent.png",
+		["unique"] = true,
+		["useable"] = true,
+		["shouldClose"] = false,   
+		["combinable"] = nil,   
+		["description"] = "Agenten Kleding"
+	},
+	
+	["politiekleding-hoofdagent"] = {
+		["name"] = "politiekleding-hoofdagent",
+		["label"] = "Politiekleding",
+		["weight"] = 100, 		
+		["type"] = "item", 		
+		["image"] = "pol_hoofdagent.png",
+		["unique"] = true,
+		["useable"] = true,
+		["shouldClose"] = false,   
+		["combinable"] = nil,   
+		["description"] = "Hoofdagenten Kleding"
+	},
+		
+	["politiekleding-brigadier"] = {
+		["name"] = "politiekleding-brigadier",
+		["label"] = "Politiekleding",
+		["weight"] = 100, 		
+		["type"] = "item", 		
+		["image"] = "pol_brigadier.png",
+		["unique"] = true,
+		["useable"] = true,
+		["shouldClose"] = false,   
+		["combinable"] = nil,   
+		["description"] = "Brigadieren Kleding"
+	},
+			
+	["politiekleding-inspecteur"] = {
+		["name"] = "politiekleding-inspecteur",
+		["label"] = "Politiekleding",
+		["weight"] = 100, 		
+		["type"] = "item", 		
+		["image"] = "pol_inspecteur.png",
+		["unique"] = true,
+		["useable"] = true,
+		["shouldClose"] = false,   
+		["combinable"] = nil,   
+		["description"] = "Inspecteuren Kleding"
+	},
+				
+	["politiekleding-hoofdinspecteur"] = {
+		["name"] = "politiekleding-hoofdinspecteur",
+		["label"] = "Politiekleding",
+		["weight"] = 100, 		
+		["type"] = "item", 		
+		["image"] = "pol_hoofdinspecteur.png",
+		["unique"] = true,
+		["useable"] = true,
+		["shouldClose"] = false,   
+		["combinable"] = nil,   
+		["description"] = "Hoofdinspecteuren Kleding"
+	},
+				
+	["politiekleding-commissaris"] = {
+		["name"] = "politiekleding-commissaris",
+		["label"] = "Politiekleding",
+		["weight"] = 100, 		
+		["type"] = "item", 		
+		["image"] = "pol_commissaris.png",
+		["unique"] = true,
+		["useable"] = true,
+		["shouldClose"] = false,   
+		["combinable"] = nil,   
+		["description"] = "Commissaris Kleding"
+	},
+					
+	["politiekleding-hoofdcommissaris"] = {
+		["name"] = "politiekleding-hoofdcommissaris",
+		["label"] = "Politiekleding",
+		["weight"] = 100, 		
+		["type"] = "item", 		
+		["image"] = "pol_hoofdcommissaris.png",
+		["unique"] = true,
+		["useable"] = true,
+		["shouldClose"] = false,   
+		["combinable"] = nil,   
+		["description"] = "Hoofdcommissaris Kleding"
+	},
+	
+	["politiekleding-eerstehoofdcommissaris"] = {
+		["name"] = "politiekleding-eerstehoofdcommissaris",
+		["label"] = "Politiekleding",
+		["weight"] = 100, 		
+		["type"] = "item", 		
+		["image"] = "pol_eerstehoofdcommissaris.png",
+		["unique"] = true,
+		["useable"] = true,
+		["shouldClose"] = false,   
+		["combinable"] = nil,   
+		["description"] = "Eerste Hoofdcommissaris Kleding"
+	},
